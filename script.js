0701360ff5c07550fd41a04bde7f90af9f581cb1
Josiah Luikham <cimmerian62@gmail.com>
2022-07-28 22:57:25 -0400

    Finish calculator html layout

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const divide = (num1, num2) => num1/num2;
 const subtract = (num1, num2) => num1-num2;
 const multiply = (num1, num2) => num1*num2;
 
-const operate = (num1, num2, op) {
+const operate = function(num1, num2, op) {
     if (op == '+') {
         return add(num1,num2);
     }
