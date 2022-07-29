2e29806fe900f6d47f74591bf142ebb6d94732f8
Josiah Luikham <cimmerian62@gmail.com>
2022-07-28 21:57:49 -0400

    Create basic JS functions

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = (num1, num2) => num1+num2;
+const divide = (num1, num2) => num1/num2;
+const subtract = (num1, num2) => num1-num2;
+const multiply = (num1, num2) => num1*num2;
+
+const operate = (num1, num2, op) {
+    if (op == '+') {
+        return add(num1,num2);
+    }
+    if (op == '/') {
+        return divide(num1,num2);
+    }
+    if (op == '-') {
+        return subtract(num1,num2);
+    }
+    if (op == '*') {
+        return multiply(num1,num2);
+    }
+}
